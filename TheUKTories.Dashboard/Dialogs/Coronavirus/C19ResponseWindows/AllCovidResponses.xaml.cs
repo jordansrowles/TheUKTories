fd32d2f86ce7c4cf9391769ed4e5d7a314a2091d
Jordan Rowles <jordansrowles@gmail.com>
2021-02-01 22:15:44 +0000

    added CovidResponse based windows to Dashboard, all working

--- TheUKTories.Dashboard/Dialogs/Coronavirus/C19ResponseWindows/AllCovidResponses.xaml.cs
+++ TheUKTories.Dashboard/Dialogs/Coronavirus/C19ResponseWindows/AllCovidResponses.xaml.cs
@@ -26,7 +26,7 @@ namespace TheUKTories.Dashboard.Dialogs.Coronavirus.C19ResponseWindows
     public partial class AllCovidResponses : AdonisWindow
     {
         CosmosDbContext _context;
-        public ObservableCollection<CovidResponses> CovidResponses { get; set; }
+        public List<CovidResponses> CovidResponses { get; set; }
 
         public AllCovidResponses(CosmosDbContext context)
         {
@@ -34,11 +34,48 @@ namespace TheUKTories.Dashboard.Dialogs.Coronavirus.C19ResponseWindows
             _context = context;
         }
 
-        private async void AdonisWindow_Loaded(object sender, RoutedEventArgs e)
+        async Task UpdateData()
         {
-            var items = await _context.GetDocumentsAsync<CovidResponses>(_context.CovidResponsesContainer);
-            CovidResponses = new ObservableCollection<CovidResponses>(items);
+            CovidResponses = await _context.GetDocumentsAsync<CovidResponses>(_context.CovidResponsesContainer);
             lvResponses.ItemsSource = CovidResponses;
         }
+
+        private async void AdonisWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            await UpdateData();
+        }
+
+        private async void btnNew_Click(object sender, RoutedEventArgs e)
+        {
+            CovidResponseWindow window = new CovidResponseWindow(_context);
+            window.ShowDialog();
+            if (window.DataChanged) 
+                await UpdateData();
+        }
+
+        private async void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            var selected = (CovidResponses)lvResponses.SelectedItems[0];
+            CovidResponseWindow window = new CovidResponseWindow(_context, selected);
+            window.ShowDialog();
+            if (window.DataChanged)
+                await UpdateData();
+        }
+
+        private async void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult results = MessageBox.Show("Ae you sure you want to delete this item?", "Confirm deletion", MessageBoxButton.YesNo);
+            if (results == MessageBoxResult.Yes)
+            {
+                var selected = (CovidResponses)lvResponses.SelectedItems[0];
+
+                await _context.DeleteItemAsync<CovidResponses>(selected.Id,
+                    new Microsoft.Azure.Cosmos.PartitionKey(selected.Date),
+                    _context.CovidResponsesContainer);
+                await UpdateData();
+            }
+        }
+
+        private async void btnRefresh_Click(object sender, RoutedEventArgs e) => await UpdateData();
     }
 }
--- TheUKTories.Dashboard/Dialogs/Coronavirus/C19ResponseWindows/CovidResponseWindow.xaml.cs
+++ TheUKTories.Dashboard/Dialogs/Coronavirus/C19ResponseWindows/CovidResponseWindow.xaml.cs
@@ -1,27 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using AdonisUI.Controls;
+using TheUKTories.DataStores.AzureCosmos;
+using TheUKTories.DataStores.AzureCosmos.Models;
+using MessageBox = AdonisUI.Controls.MessageBox;
+using MessageBoxButton = AdonisUI.Controls.MessageBoxButton;
+using MessageBoxResult = AdonisUI.Controls.MessageBoxResult;
 
 namespace TheUKTories.Dashboard.Dialogs.Coronavirus.C19ResponseWindows
 {
     /// <summary>
     /// Interaction logic for CovidResponseWindow.xaml
     /// </summary>
-    public partial class CovidResponseWindow : Window
+    public partial class CovidResponseWindow : AdonisWindow
     {
-        public CovidResponseWindow()
+        CosmosDbContext _context;
+        public CovidResponses CovidResponse { get; set; }
+        public bool DataChanged { get; set; }
+
+        public CovidResponseWindow(CosmosDbContext context)
         {
             InitializeComponent();
+            _context = context;
+            CovidResponse = new CovidResponses() { Id = Guid.NewGuid().ToString() };
+            DataChanged = false;
+        }
+
+        public CovidResponseWindow(CosmosDbContext context, CovidResponses response)
+        {
+            InitializeComponent();
+            _context = context;
+            CovidResponse = response;
+            ctrlSources.SourceItems = response.Sources;
+            DataChanged = false;
+
+            tbString.Text = response.String;
+            tbDate.Text = response.Date;
+        }
+
+        private async void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            if (Shared.ValidateListOfControls(true, tbString, tbDate))
+            {
+                CovidResponse.String = tbString.Text;
+                CovidResponse.Date = tbDate.Text;
+                CovidResponse.Sources = ctrlSources.SourceItems;
+
+                await _context.UpsertAsync<CovidResponses>(CovidResponse,
+                    new Microsoft.Azure.Cosmos.PartitionKey(CovidResponse.Date),
+                    _context.CovidResponsesContainer);
+                DataChanged = true;
+                Close();
+            }
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show(
+                "Are you sure you want to cancel? All changes or potential additions could be lost.",
+                "Sure?", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+                Close();
         }
     }
 }
